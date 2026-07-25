89959d3875a460c5cc48ef0a0aabd87e0c5d347f
AkshayKarthickMS <akshaykarthick486@gmail.com>
2026-07-25 18:33:33 +0530

    First Occurence - KMP Optimal

--- Knuth-Morris-Pratt Algorithm.java
+++ Knuth-Morris-Pratt Algorithm.java
@@ -5,17 +5,48 @@ class Solution {
         int inner = pat.length();
         
         if (inner > outer) return -1;
+
+        int[] lps = new int[inner];
+        lps[0] = 0;
+        int prevLPS = 0;
+        int i = 1;
         
-        for (int i = 0; i <= outer-inner; i++){
-            boolean match = true;
-            for (int j = 0; j < inner; j++){
-                if (txt.charAt(i+j) != pat.charAt(j)){
-                    match = false;
-                    break;
+        while (i < inner){
+            if (pat.charAt(i) == pat.charAt(prevLPS)){
+                prevLPS++;
+                lps[i] = prevLPS;
+                i++;
+            }
+            else if (prevLPS == 0){
+                lps[i] = 0;
+                i++;
+            }
+            else {
+                prevLPS = lps[prevLPS - 1];
+            }
+        }
+        
+        i = 0;
+        int j = 0;
+        
+        while (i < outer){
+            if (txt.charAt(i) == pat.charAt(j)){
+                i++;
+                j++;
+            }
+            else {
+                if (j == 0){
+                    i++;
+                }
+                else {
+                    j = lps[j-1];
                 }
             }
-            if (match) return i;
+            if (j == inner){
+                return i - j;
+            }
         }
+
     return -1;
     }
 }
