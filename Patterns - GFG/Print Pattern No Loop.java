8f368063a90289bc83015b442f0686c93dc1003a
AkshayKarthickMS <akshaykarthick486@gmail.com>
2026-07-29 13:50:12 +0530

    Optimal Recursive

--- Patterns - GFG/Print Pattern No Loop.java
+++ Patterns - GFG/Print Pattern No Loop.java
@@ -2,34 +2,21 @@ class Solution {
     public ArrayList<Integer> pattern(int n) {
         // code here
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(n);
-        if (n <= 0){
-            return list;
-        } else{
-            dec_func(list,n,n);
-        }
+        
+        result(list,n);
         return list;
     }
-    public void dec_func(ArrayList<Integer> list, int num, int n){
-        
-        num = num-5;
-        list.add(num);
-        
-        if (num > 0){
-            dec_func(list,num,n);
-        }else{
-            inc_func(list,num,n);
-        }
-        return;
-    }
-    public void inc_func(ArrayList<Integer> list, int num, int n){
-        num = num + 5;
-        list.add(num);
-        
-        if (num != n){
-            inc_func(list,num,n);
-        }else{
+    
+    private void result(ArrayList<Integer> list, int num){
+        if (num<=0){
+            list.add(num);
             return;
         }
+        
+        list.add(num);
+        
+        result(list,num-5);
+        
+        list.add(num);
     }
 }
