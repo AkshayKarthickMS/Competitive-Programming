be1f01557bf06a0edf2f79585de0b83116cfb979
AkshayKarthickMS <akshaykarthick486@gmail.com>
2026-08-29 13:00:05 +0530

    Optimal using StringBuilder

--- Patterns - GFG/Look and say.java
+++ Patterns - GFG/Look and say.java
@@ -2,36 +2,27 @@ class Solution {
     public String countAndSay(int n) {
         // code here
         
-        String s = "11";
-          if (n == 1){
-              return "1";
-          }
-           else if (n == 2){
-               return "11";
-           }
-           else {
+        String s = "1";
+
                
-               for (int i = 3; i <= n; i++){
+               for (int i = 2; i <= n; i++){
                    char c = s.charAt(0);
                    int cnt = 1;
-                   String temp = "";
+                   StringBuilder temp = new StringBuilder();
                    for (int j = 1; j < s.length(); j++){
                        if (c == s.charAt(j)){
                            cnt++;
                        }
                        else{
-                           temp = temp + Integer.toString(cnt) + c;
+                           temp.append(cnt).append(c);
                            c = s.charAt(j);
                            cnt = 1;
                        }
                    }
-                   temp = temp + Integer.toString(cnt) + c;
-                   s = temp;
+                   temp.append(cnt).append(c);
+                   s = temp.toString();
                }
                
-               
-               
-           }
         return s;
     }
 }
