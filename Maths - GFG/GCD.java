c287a5e2bd2702be50ebcafbadb38acb3b5f0ec0
AkshayKarthickMS <akshaykarthick486@gmail.com>
2026-09-02 22:00:52 +0530

    Brute

---
+++ Maths - GFG/GCD.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int gcd(int a, int b) {
+        // code here
+        while (a!=b){
+            if (a > b){
+                a = a - b;
+            }
+            else{
+                b = b - a;
+            }
+        }
+        return a;
+    }
+}
