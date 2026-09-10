919a9e3762419c58b2b339cbe9856b16510e2170
AkshayKarthickMS <akshaykarthick486@gmail.com>
2026-09-10 19:34:22 +0530

    Solution

---
+++ Maths - GFG/cnt perfect square.java
@@ -0,0 +1,10 @@
+class Solution {
+    static int countSquares(int n) {
+        // code here
+        int cnt = 0;
+        for (int i = 1; i * i < n; i++){
+            cnt++;
+        }
+        return cnt;
+    }
+}
