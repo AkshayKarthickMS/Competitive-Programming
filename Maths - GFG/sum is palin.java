6bddeb0af20ef4b47a42a58e67d51e894d581b6c
AkshayKarthickMS <akshaykarthick486@gmail.com>
2026-09-10 19:47:43 +0530

    Optimal - multiple of 11

--- Maths - GFG/sum is palin.java
+++ Maths - GFG/sum is palin.java
@@ -1,21 +1,15 @@
 class Solution {
     boolean isDigitSumPalindrome(int n) {
-        // code here
         int sum = 0;
         
-        while (n > 0){
+        // Calculate the sum of the digits
+        while (n > 0) {
             sum += n % 10;
             n /= 10;
         }
         
-        int temp = sum;
-        
-        int pal = 0;
-        
-        while (temp > 0){
-            pal = (pal * 10) + temp % 10;
-            temp /= 10;
-        }
-        return sum == pal;
+        // Since the maximum sum for n <= 10^9 is 81 (for 999999999),
+        // it is a palindrome if it's a single digit or a multiple of 11.
+        return sum < 10 || sum % 11 == 0;
     }
 }
